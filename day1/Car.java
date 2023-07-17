1f5c65c129c41a1beecf60e6027814a9aa71279f
csramya123 <139791710+csramya123@users.noreply.github.com>
2023-07-17 21:50:46 +0530

    Add files via upload
    Day1

---
+++ day1/Car.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int price=150000;		
+		String brandName="KIA";		
+		char logo='K';		
+		float fuel=15.45f;	
+		double modelVariant=3456.4358435;
+		boolean isNew=false;		
+		long contact=987654321023l;
+		
+		System.out.println("The brand name of the Car : "+brandName);
+		System.out.println("Car price : "+price);
+		System.out.println("Car logo is : "+logo);		
+		System.out.println("Fuel Capacity : "+fuel);
+		System.out.println(isNew);
+		System.out.println(modelVariant);
+		System.out.println("Phone No. : "+contact);
+
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=100.2f;
+		String browserName="Firefox";
+		boolean isVisible=true;
+		int year=1998;
+		char logo='M';
+		
+                System.out.println("Browser Version : "+version);
+                System.out.println("Browser Name : "+browserName);
+				System.out.println( "Browser is Visible : "+isVisible);
+				System.out.println("Manufacture Year : "+year);
+				System.out.println("Browser Logo : "+logo);
+				
+				
+
+	}
+
+}
